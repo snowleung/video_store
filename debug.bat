c8084b78a120fb2dcb2a9cee8a883387f7b288ce
snowleung <snowleung@gmail.com>
2013-09-16 20:27:47 +0800

    add debug

--- debug.bat
+++ debug.bat
@@ -5,6 +5,7 @@ taskkill /F /IM nginx.exe > nul
 cd /d %~dp0
 
 xcopy /y /e nginx-1.4.2\script\build_index.py d:\websites\nginx-1.4.2\script\
+
 xcopy /y /e nginx-1.4.2\template\bt.htm d:\websites\nginx-1.4.2\template\
 
 d:
@@ -14,5 +15,7 @@ cd d:\websites\nginx-1.4.2\
 del /Q  html\*
 
 video_ser.bat
+
+cd /d %~dp0
 
 pause
