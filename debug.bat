78f5ba43e0902f5af6b0368ae825683a1fc67f36
snowleung <snowleung@gmail.com>
2013-09-16 20:08:05 +0800

    remove old file

--- debug.bat
+++ debug.bat
@@ -5,10 +5,13 @@ taskkill /F /IM nginx.exe > nul
 cd /d %~dp0
 
 xcopy /y /e nginx-1.4.2\script\build_index.py d:\websites\nginx-1.4.2\script\
+xcopy /y /e nginx-1.4.2\template\bt.htm d:\websites\nginx-1.4.2\template\
 
 d:
 
 cd d:\websites\nginx-1.4.2\
+
+del /Q  html\*
 
 video_ser.bat
 
